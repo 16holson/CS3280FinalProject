0f56499f8b3932d48358c90730bd2a285bed7636
levibernards <112440727+levibernards@users.noreply.github.com>
2022-11-07 19:14:22 -0700

    Main Window Screen Update

--- obj/Debug/net6.0-windows/App.g.i.cs
+++ obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace CS3280FinalProject {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace CS3280FinalProject {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/CS3280FinalProject;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CS3280FinalProject;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace CS3280FinalProject {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
         public static void Main() {
             CS3280FinalProject.App app = new CS3280FinalProject.App();
             app.InitializeComponent();
