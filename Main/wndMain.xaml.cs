59a11803f141e4030dee419d6b322c5d5f1ea0c3
levibernards <112440727+levibernards@users.noreply.github.com>
2022-11-14 12:24:31 -0700

    Screen and UI Updates

--- Main/wndMain.xaml.cs
+++ Main/wndMain.xaml.cs
@@ -14,6 +14,7 @@ using CS3280FinalProject.Items;
 using CS3280FinalProject.Search;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -35,6 +36,8 @@ namespace CS3280FinalProject.Main
         List<Shared.Item> lItemList;
 
         clsMainLogic logic;
+
+        Shared.Invoice currentInvoice;
         #endregion
 
         #region Functions
@@ -55,50 +58,91 @@ namespace CS3280FinalProject.Main
 
         #region UI Functions
 
-        //Created by Braxton Wright
         /// <summary>
-        /// This event listener listens for when the user clicks the Search item inside the menu.  It will then take them to the Search window.
+        /// (Braxton Wright, created in order to let his portion be completed)
+        /// This event listener listens for when the user clicks the Search item inside the menu.  
+        /// It will then take them to the Search window.
+        /// 
+        /// (Levi Bernards, Created to perform operations on the Main Window)
+        /// Upon closing the Search screen, The program will copy the selected invoice
+        /// into the currentInvoice object.  It will then update the screen by changing
+        /// Labels, allowing the Edit Invoice button to be pressed and displaying the
+        /// items in the DataGrid
         /// </summary>
         /// <param name="sender">The object that called the event.</param>
         /// <param name="e">Contains the event data for the event.</param>
         private void MenuSearch_Click(object sender, RoutedEventArgs e)
         {
-            wndSearch Search = new wndSearch();
-            Search.Owner = this;  //this sets it so that the search window owner is this window (so it loads where this window is currently)
+            try
+            {
+                wndSearch Search = new wndSearch();
+                Search.Owner = this;  //this sets it so that the search window owner is this window (so it loads where this window is currently)
 
-            this.Hide();  //hide this from the user
-            Search.ShowDialog();  //open the search window and pause here in the code until the window is closed
-            this.Show();  //shows this window to the user
+                this.Hide();  //hide this from the user
+                Search.ShowDialog();  //open the search window and pause here in the code until the window is closed
+                this.Show();  //shows this window to the user
 
-            // This is how the user gets the chosen invoice num from the user
-            selectedInvoiceNum = Search.selectedNum;
+                // This is how the user gets the chosen invoice num from the user
+                if (Search.selectedNum != null)
+                {
+                    selectedInvoiceNum = Search.selectedNum;
+                    updateScreen(selectedInvoiceNum);
 
-            InvoiceNumLabel.Content = "Invoice: " + selectedInvoiceNum;
+                    //Allow the option to edit the invoice
+                    EditInvoiceButton.IsEnabled = true;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
         }
 
-        //Created by Braxton Wright
         /// <summary>
-        /// This event listener listens for when the user clicks the Edit Items item inside the menu.  It will then take them to the Items window.
+        /// (Braxton Wright, Created to allow the window to open when button is clicked)
+        /// This event listener listens for when the user clicks the Edit Items item inside the menu.  
+        /// It will then take them to the Items window.
+        /// 
+        /// (Levi Bernards, Main Window Functions)
+        /// If any items are changed, the main window will update
+        /// the Item Combo Box and if there is a current invoice being displayed,
+        /// The item information regarding that will be updated as well
         /// </summary>
         /// <param name="sender">The object that called the event.</param>
         /// <param name="e">Contains the event data for the event.</param>
         private void MenuEditItems_Click(object sender, RoutedEventArgs e)
         {
-            wndItems EditItems = new wndItems();
-            EditItems.Owner = this;  //this sets it so that the edit items window owner is this window (so it loads where this window is currently)
-
-            this.Hide();  //hide this from the user
-            //open the edit items window and pause here in the code until the window is closed (the stuff before the "EditItems.ShowDialog()" catches the dialogresult the window returns to determine if any items have been modified)
-            EditItems.ShowDialog();
-            this.Show();  //shows this window to the user
-
-            bItemsChanged = EditItems.HasItemsChanged;
-
-            //see if there has been any changes to the items inside the DB so you can see if you are required to update this window's list of items
-            if (bItemsChanged == true)
+            try
             {
-                //perform an update/reassign to your list
+                wndItems EditItems = new wndItems();
+                EditItems.Owner = this;  //this sets it so that the edit items window owner is this window (so it loads where this window is currently)
+
+                this.Hide();  //hide this from the user
+                              //open the edit items window and pause here in the code until the window is closed (the stuff before the "EditItems.ShowDialog()" catches the dialogresult the window returns to determine if any items have been modified)
+                EditItems.ShowDialog();
+                this.Show();  //shows this window to the user
+
+                bItemsChanged = EditItems.HasItemsChanged;
+
+                //see if there has been any changes to the items inside the DB so you can see if you are required to update this window's list of items
+                if (bItemsChanged == true)
+                {
+                    //Refill Combo Box
+                    fillItemCB();
+                    if (selectedInvoiceNum != null)
+                    {
+                        updateScreen(selectedInvoiceNum);
+                    }
+                }
             }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
         }
 
 
@@ -113,7 +157,12 @@ namespace CS3280FinalProject.Main
                 SaveInvoiceButton.IsEnabled = true;
 
                 // Show TBD as the Invoice Num
-                InvoiceNumLabel.Content = "Invoice #: TBD";
+                InvoiceNumLabel.Content = "Invoice Num: TBD";
+                TotalCostLabel.Content = "Total Cost: __";
+                if(currentInvoice != null)
+                {
+                    currentInvoice = new Shared.Invoice();
+                }
 
                 // Enable the Combo Box for user use
                 InvoiceItemComboBox.IsEnabled = true;
@@ -174,12 +223,90 @@ namespace CS3280FinalProject.Main
 
         #region Screen Functions
 
+        /// <summary>
+        /// Empties and fills the General Item List
+        /// It then casts it onto the ItemComboBox
+        /// </summary>
         public void fillItemCB()
         {
-            lItemList = logic.ItemList();
-            InvoiceItemComboBox.ItemsSource = lItemList;
+            try
+            {
+                if(lItemList != null)
+                {
+                    lItemList.Clear();
+                }
+                lItemList = logic.ItemList();
+                InvoiceItemComboBox.ItemsSource = lItemList;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
+        /// <summary>
+        /// Updates Invoice Num, Date and Total Cost
+        /// Labels in the main screen
+        /// </summary>
+        /// <param name="currentInvoice">Shared.Invoice currentInvoice</param>
+        public void updateInvoiceLabels(Shared.Invoice currentInvoice)
+        {
+            try
+            {
+                //Update Labels
+                InvoiceNumLabel.Content = "Invoice: " + selectedInvoiceNum;
+                //Update date
+                TotalCostLabel.Content = "Total Cost: $" + currentInvoice.totalCost;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Displays the selected invoice on the screen
+        /// </summary>
+        /// <param name="itemList">List of items in the invoice</param>
+        public void updateDataGrid(List <Shared.Item> itemList)
+        {
+            try
+            {
+                MainItemDisplay.ItemsSource = itemList;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Saves a copy of the current invoice. then it updates the
+        /// screen by callilng updateInvoiceLabels() and updateDataGrid()
+        /// </summary>
+        /// <param name="sInvoiceNum">string currentInvoiceNum</param>
+        public void updateScreen(string sInvoiceNum)
+        {
+            try
+            {
+                //Update the Datagrid with the invoice selected
+                currentInvoice = logic.GetInvoice(selectedInvoiceNum);
+                currentInvoice.items = logic.GetAllItems(selectedInvoiceNum);
+
+                //Update Screen
+                updateInvoiceLabels(currentInvoice);
+                updateDataGrid(currentInvoice.items);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
         #endregion
 
 
